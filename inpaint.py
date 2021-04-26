8114e9c311feb1b116ed3c2abfcfce1769713b43
CodePerson2 <mattiasstroman@gmail.com>
2021-04-25 22:29:03 -0700

    mostly finished

--- inpaint.py
+++ inpaint.py
@@ -1,56 +1,86 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
+# CMPT340 Inpainting using PCA
+# Original use: https://www.kaggle.com/drikee/tutorial-pca-intuition-and-image-completion
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+# imports
+import numpy as np
+import pandas as pd
+# plotting
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn import decomposition
 from sklearn.linear_model import RidgeCV
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+
 import time
 import os
-#print(os.listdir("C:/Users/matti/Downloads/archive/images-224/images-224/"))
 import warnings
+import pathlib
+path = pathlib.Path(__file__).parent.absolute()
 warnings.filterwarnings('ignore')
-# Any results you write to the current directory are saved as output.
 
-x_load = np.load('C:/Users/matti/Downloads/archive/images-224/X.npy')
-y_load = np.load('C:/Users/matti/Downloads/archive/images-224/Y.npy')
+def printImg():
+    for each in range(4):
+        plt.figure(figsize=(8, 5))
+
+        plt.imshow(x_load[each])
+        plt.axis('off')
+        title = "Sample " + str(each)
+        plt.title(title)
+    plt.show()
+
+
+def MakeData(x_load, image_index_list):
+    X = x_load.reshape((len(x_load), -1))
+    train = X
+    test = X[image_index_list]
+    n_pixels = X.shape[1]
+    # Upper half of the chest
+    X_train = train[:, :(n_pixels + 1) // 2]
+    X_test = test[:, :(n_pixels + 1) // 2]
+    # Lower half of the chest
+    y_train = train[:, n_pixels // 2:]
+    y_test = test[:, n_pixels // 2:]
+
+    print("Dimensions of training data" + str(X_train.shape))
+    
+
+    return X, X_train, y_train, X_test, y_test
+
+#PCA of RidgeVC regression
+def pca(X):
+    # dimensions
+    (n_samples, n_features) = X.shape
+    estimator = decomposition.PCA(
+        n_components=4, svd_solver='randomized', whiten=True)
+    estimator.fit(X)
+    components_ = estimator.components_
+    images = components_[:4]
+    plt.figure(figsize=(6, 5))
+    for i, comp in enumerate(images):
+        vmax = max(comp.max(), -comp.min())
+        plt.imshow(comp.reshape((64, 64)),
+                interpolation='nearest', vmin=-vmax, vmax=vmax)
+
+        # ploting data
+        plt.xticks(())
+        plt.yticks(())
+    plt.title("Principle Component Analysis")
+    plt.savefig('PCA.png')
+    plt.show()
+
+# loading the path to numpy files
+# change if required
+x_load = np.load(str(path) + '/X.npy')
 
 img_size = 64
-print('X_data shape:', np.array(x_load).shape)
+print('X shape:', np.array(x_load).shape)
 image_index_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# one sample from each digits
-# for each in range(4):
-#     plt.figure(figsize=(8,5))
-    
-#     plt.imshow(x_load[each])
-#     plt.axis('off')
-#     title = "Sign " + str(each) 
-#     plt.title(title)
-# plt.show()
+# print example of data used
+printImg()
 
-
-
-X = x_load.reshape((len(x_load), -1)) 
-train = X
-test = X[image_index_list]
-n_pixels = X.shape[1]
-# Upper half of the faces
-X_train = train[:, :(n_pixels + 1) // 2]
-X_test = test[:, :(n_pixels + 1) // 2]
-# Lower half of the faces
-y_train = train[:, n_pixels // 2:]
-y_test = test[:, n_pixels // 2:]
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
-
+# get all arrays for the synthesis of data
+X, X_train, y_train, X_test, y_test = MakeData(x_load, image_index_list)
 
 
 # Fit estimators
@@ -58,56 +88,45 @@ estimator = RidgeCV()
 start = time.time()   # returns in second
 estimator.fit(X_train, y_train)
 end = time.time()
-print("Training time is "+ str(end - start) + " second.")
+print("Time to Train " + str(end - start) + " second.")
 start = time.time()   # returns in second
 y_test_predict = estimator.predict(X_test)
 end = time.time()
-print("Prediction time is "+ str(end - start) + " second.")
+print("Time to predict " + str(end - start) + " second.")
 
 
-
-# Plot the completed faces
-image_shape = (64, 64)
+# Plot the completed xrays
 n_faces = 10
 n_cols = 1
 
 
-image_shape = (64, 64)
+shape = (64, 64)
 plt.figure(figsize=(8, 24))
 for i in range(10):
     true_digits = np.hstack((X_test[i], y_test[i]))
-    if i:
+
+    # plotting data, gives title to entry 1
+    if i > 0:
         sub = plt.subplot(10, 2, i * 2 + 1)
     else:
-        sub = plt.subplot(10, 2, i * 2 + 1, title="true digits")
-    
-    sub.imshow(true_digits.reshape(image_shape),interpolation="nearest")
+        sub = plt.subplot(10, 2, i * 2 + 1, title="True Image")
+
+    sub.imshow(true_digits.reshape(shape), interpolation="nearest")
     sub.axis("off")
     completed_digits = np.hstack((X_test[i], y_test_predict[i]))
 
-    if i:
-        sub = plt.subplot(10, 2, i * 2 + 2 )
+    # plotting data, gives title to entry 1
+    if i > 0:
+        sub = plt.subplot(10, 2, i * 2 + 2)
 
     else:
-        sub = plt.subplot(10, 2, i * 2 + 2,title="RidgeCV")
+        sub = plt.subplot(10, 2, i * 2 + 2, title="Lower Half Inpainted")
 
-    sub.imshow(completed_digits.reshape(image_shape),interpolation="nearest")
+    sub.imshow(completed_digits.reshape(shape), interpolation="nearest")
     sub.axis("off")
 
 plt.show()
 
 
-n_components = 5
-(n_samples, n_features) = X.shape
-estimator = decomposition.PCA(n_components=n_components, svd_solver='randomized', whiten=True)
-estimator.fit(X)
-components_ = estimator.components_
-images = components_[:n_components]
-plt.figure(figsize=(6, 5))
-for i, comp in enumerate(images):
-    vmax = max(comp.max(), -comp.min())
-    plt.imshow(comp.reshape((64, 64)),interpolation='nearest',vmin=-vmax, vmax=vmax)
-    plt.xticks(())
-    plt.yticks(())
-plt.savefig('graph.png')
-plt.show()
+#show what pca is seeing
+pca(X)
--- makeNum.py
+++ makeNum.py
@@ -1,21 +1,34 @@
+# File makeing algorithm for PCA inpainting
+# CMPT340 Project
+
 import cv2
 import glob
 import numpy as np
 
-X_data = []
-files = glob.glob ("C:/Users/matti/Downloads/archive/images-224/images-224/*.PNG")
+# get path to images that will be put into numpy file
+import pathlib
+path = pathlib.Path(__file__).parent.absolute()
+
+data = []
+files = glob.glob(
+    str(path) + "/trainingData/images-224/*.PNG")
 count = 0
+
+# rgb weights
 rgb_weights = [0.2989, 0.5870, 0.1140]
 
 for myFile in files:
     if count > 2000:
         break
     print(myFile)
-    image = cv2.imread (myFile)
-    image = np.dot(image[...,:3], rgb_weights)
-    X_data.append (cv2.resize(image, (64, 64), interpolation=cv2.INTER_CUBIC))
+    im = cv2.imread(myFile)
+    im = np.dot(im[..., :3], rgb_weights)
+
+    # push image to npy file
+    data.append(cv2.resize(im, (64, 64), interpolation=cv2.INTER_CUBIC))
     count += 1
 
-print('X_data shape:', np.array(X_data).shape)
+print('X_data shape:', np.array(data).shape)
 
-np.save("C:/Users/matti/Downloads/archive/images-224/X.npy", X_data)
+# store npy file
+np.save(str(path) + "/X.npy", data)
